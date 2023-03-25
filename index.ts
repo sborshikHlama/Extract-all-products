4636e727a920c592ccba195cb27f213ebe6fd26f
sborshikHlama <logrus.928@gmail.com>
2023-03-25 10:59:28 +0100

    delete defineRanges function

--- index.ts
+++ index.ts
@@ -14,26 +14,10 @@ interface ResponseData {        // For example:
 
 const url = 'https://api.ecommerce.com/products' // API endpoint 
 
-// The first step is to deside in which price range request func will search products
-function defineRanges(range: number): PriceRangesArray { 
-    const priceRangesArray: PriceRangesArray = []
-    const maximumCost = 100000 // Costs somewhere between $0 and $100,000 
-    const allRanges = maximumCost / range  // Split Maximium on price range to get all ranges
-    for(let i = 0; i < allRanges; i++) { 
-        let minPrice = i * range 
-        let maxPrice = (i + 1) * range 
-        priceRangesArray.push({
-            minPrice: minPrice,
-            maxPrice: maxPrice
-        })
-    }
-    return priceRangesArray
-}
-
-// The second step is to create request function wich return array of products from range 
+//Create request function wich return array of products from range 
 async function sendRequest(minPrice: number, maxPrice: number): Promise<Array<Product>> {
   try {
-    const response = await fetch(`${url}?price-range=${minPrice}-${maxPrice}`)
+    const response = await fetch(`${url}?${minPrice}&${maxPrice}`)
     const responseData: ResponseData = await response.json() // Parse json
     const products: Array<Product> = accumulateProducts(responseData)
     return products
@@ -54,20 +38,24 @@ function accumulateProducts(data: ResponseData): Array<Product> {
     return products
 }
 
-// The third step is to extract all products
+// Extract all products
 async function extractProducts(range: number): Promise<Array<Product>> {
     const allProducts: Array<Product> = []
-    const priceRanges = defineRanges(range)
+    
     try {
         const response = await fetch(url) // Make request to find out total amount products
         const responseData: ResponseData = await response.json() 
-        const total = responseData.total  
-    
+        const total = responseData.total
+        const maximumCost = 100000 // Costs somewhere between $0 and $100,000 
+        const allRanges = maximumCost / range  // Split Maximium on price range to get all ranges
+
         // Send requests to get all products from all ranges
-        for(let i = 0; i < priceRanges.length; i++) {
-            const result: Array<Product> = await sendRequest(priceRanges[i].minPrice, priceRanges[i].maxPrice) 
-            allProducts.push(...result) // Prevent allProducts from being 2D array
-        }
+        for(let index = 0; index < allRanges; index++) { 
+            let minPrice = index * range 
+            let maxPrice = (index + 1) * range 
+            let result: Array<Product> = await sendRequest(minPrice, maxPrice)
+            allProducts.push(...result)
+        }  
     
         if(allProducts.length === total) { //Make sure that all products were saved
             console.log('All products have been extracted')
